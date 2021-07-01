86de7580f2de392aeda77cf4b5a88d6746295997
JJSanch <jon.sanchez003@gmail.com>
2021-07-01 16:18:32 -0700

    front end first commit

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,7 +1,7 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-import 'bootstrap/dist/css/bootstrap/min.css'
+// import 'bootstrap/dist/css/bootstrap/min.css'
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
---
+++ app/javascript/components/pages/__tests__/Home.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import ApartmentIndex from './Home'
+
+
+Enzyme.configure({ adapter: new Adapter()})
+
+describe("When Home renders...", () => {
+  it("displays a heading", () => {
+    const catIndex = shallow(<ApartmentIndex />)
+    const indexHeading = ApartmentIndex.find("h1")
+    expect(ApartmentIndex.text()).toEqual("Welcome To Apartment App")
+  })
+})
